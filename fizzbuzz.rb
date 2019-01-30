34897df723dd140d5eeb92671f5b232f93aa0591
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 04:24:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 15 == 0
+    puts "FizzBuzz"
+  else
+    puts nil     
 end
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz()
+fizzbuzz(23)

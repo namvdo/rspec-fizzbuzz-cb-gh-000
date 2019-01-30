5af6271aaa3a40326d08558fed6a21e5248f32f8
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 04:35:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require_relative './spec/fizzbuzz_spec.rb'
+require_relative './spec/spec_helper.rb'
 def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"

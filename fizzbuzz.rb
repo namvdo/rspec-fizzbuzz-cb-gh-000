0fce272fc5304a5f3fe3b4d7b9fa19fcdc21074a
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 04:38:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,14 @@
 require_relative './spec/fizzbuzz_spec.rb'
 require_relative './spec/spec_helper.rb'
 def fizzbuzz(int)
-  if int % 3 == 0
-    return "Fizz"
+  if int % 15 == 0
+    return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 15 == 0
-    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
   else
     return nil
   end
 end
+
